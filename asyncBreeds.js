6303d4c2c245788585d7ca9b88cf7b9c94434b36
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-26 05:15:54 +0000

    Implemented asynchronous return values assignment

---
+++ asyncBreeds.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) callback(data);
+  });
+};
+
+const bombay = breedDetailsFromFile('Bombay', (bombay) => console.log('Return Value: ',bombay));
+console.log('Return Value: ', bombay);
